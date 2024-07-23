316397dfb6b35a1f9aa342f79f80c586f1cedc3d
Guilherme <pepe.ks.rx.1@gmail.com>
2024-07-22 22:08:05 -0300

    Criando as classes Bootcamp e Dev

---
+++ src/main/java/br/com/bootcamp/Bootcamp.java
@@ -0,0 +1,68 @@
+package br.com.bootcamp;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+    private String nome;
+    private  String descricao;
+    private final LocalDate dataIncial = LocalDate.now();
+    private final LocalDate dataFinal = dataIncial.plusDays(45);
+    private Set<Dev> devInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new HashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataIncial() {
+        return dataIncial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public Set<Dev> getDevInscritos() {
+        return devInscritos;
+    }
+
+    public void setDevInscritos(Set<Dev> devInscritos) {
+        this.devInscritos = devInscritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataIncial, bootcamp.dataIncial) && Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(devInscritos, bootcamp.devInscritos) && Objects.equals(conteudos, bootcamp.conteudos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, descricao, dataIncial, dataFinal, devInscritos, conteudos);
+    }
+}
