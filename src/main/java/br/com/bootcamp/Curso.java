a4c28eb88eeaede0cac88b6fcbc68363d33c27cd
Guilherme <pepe.ks.rx.1@gmail.com>
2024-07-22 22:07:32 -0300

    Criando a classe mentoria, conteudo e curso

---
+++ src/main/java/br/com/bootcamp/Curso.java
@@ -0,0 +1,33 @@
+package br.com.bootcamp;
+
+import java.time.LocalDate;
+
+public class Curso extends Conteudo {
+
+    private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "carga horaria=" + cargaHoraria +
+                ", titulo=" + getTitulo() +
+                ", descricao=" + getDescricao() +
+                '}';
+    }
+}
